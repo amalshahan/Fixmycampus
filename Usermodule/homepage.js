ae0a5ff660cdcd14de57018717ac70e9030b943a
amalshahan <amalshahan83@gmail.com>
2025-03-24 02:40:51 -0700

    Add files via upload

---
+++ Usermodule/homepage.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
+import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAntn_AszKaERCxDJaYr1AqYulMSUyQVv8",
+  authDomain: "fixmycampus-81313.firebaseapp.com",
+  projectId: "fixmycampus-81313",
+  storageBucket: "fixmycampus-81313.firebasestorage.app",
+  messagingSenderId: "487715498876",
+  appId: "1:487715498876:web:dbb370d680ac56b6c312f9"
+  };
+ 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const auth=getAuth();
+  const db=getFirestore();
+
+  
+  const logoutButton=document.getElementById('logout');
+
+  logoutButton.addEventListener('click',()=>{
+    localStorage.removeItem('loggedInUserId');
+    signOut(auth)
+    .then(()=>{
+        window.location.href='index.html';
+    })
+    .catch((error)=>{
+        console.error('Error Signing out:', error);
+    })
+  })
